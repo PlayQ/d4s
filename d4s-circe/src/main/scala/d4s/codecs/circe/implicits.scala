65f1970607f0bd10529a186594d111240a791505
Vladyslav Podilnyk <vpodilnyk@playq.net>
2020-04-06 18:55:35 +0300

    implicit converson from circe codecs to magnolia ones.

---
+++ d4s-circe/src/main/scala/d4s/codecs/circe/implicits.scala
@@ -0,0 +1,11 @@
+package d4s.codecs.circe
+
+import d4s.codecs.{D4SAttributeEncoder, D4SDecoder}
+import io.circe.{Decoder, Encoder}
+
+import scala.language.implicitConversions
+
+object implicits {
+  implicit def fromEncoder[T](enc: Encoder[T]): D4SAttributeEncoder[T] = deriveAttribute[T](enc)
+  implicit def fromDecoder[T](dec: Decoder[T]): D4SDecoder[T]          = deriveDecoder[T](dec)
+}
