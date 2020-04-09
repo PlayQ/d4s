808aa8cb45a1629e10f0b50d192af861caf3efc0
Vladyslav Podilnyk <vpodilnyk@playq.net>
2020-04-09 19:32:12 +0300

    implicit conversion to d4s encoder.

--- d4s-circe/src/main/scala/d4s/codecs/circe/implicits.scala
+++ d4s-circe/src/main/scala/d4s/codecs/circe/implicits.scala
@@ -1,11 +1,12 @@
 package d4s.codecs.circe
 
-import d4s.codecs.{D4SAttributeEncoder, D4SDecoder}
+import d4s.codecs.{D4SAttributeEncoder, D4SDecoder, D4SEncoder}
 import io.circe.{Decoder, Encoder}
 
 import scala.language.implicitConversions
 
 object implicits {
   implicit def fromEncoder[T](enc: Encoder[T]): D4SAttributeEncoder[T] = deriveAttribute[T](enc)
+  implicit def fromEncoder[T](enc: Encoder.AsObject[T]): D4SEncoder[T] = deriveEncoder(enc)
   implicit def fromDecoder[T](dec: Decoder[T]): D4SDecoder[T]          = deriveDecoder[T](dec)
 }
