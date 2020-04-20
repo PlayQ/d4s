b6e11283fd9a15beb434fdff283ec90bd1b7b4f2
Kai <450507+neko-kai@users.noreply.github.com>
2020-04-20 17:39:54 +0100

    add D4SAttributeCodec (#32)

---
+++ d4s-circe/src/main/scala/d4s/codecs/circe/D4SCirceAttributeCodec.scala
@@ -0,0 +1,8 @@
+package d4s.codecs.circe
+
+import d4s.codecs.{D4SAttributeCodec, D4SCodec}
+import io.circe.{Decoder, Encoder}
+
+object D4SCirceAttributeCodec {
+  def derived[T: Encoder: Decoder]: D4SAttributeCodec[T] = D4SAttributeCodec.fromPair(D4SCirceAttributeEncoder.derived, D4SCirceDecoder.derived)
+}
--- d4s/src/main/scala/d4s/codecs/WithD4S.scala
+++ d4s/src/main/scala/d4s/codecs/WithD4S.scala
@@ -7,15 +7,26 @@ abstract class WithD4S[T]()(implicit derivedCodec: D4SDerivedCodec[T]) {
   implicit val codec: D4SCodec[T] = D4SCodec.fromPair(derivedCodec.enc, derivedCodec.dec)
 }
 
-final class MaterializeDerivationMacros(val c: blackbox.Context) {
-  import c.universe._
+object MaterializeDerivationMacros {
 
-  def materializeCodec[A: c.WeakTypeTag]: c.Expr[D4SDerivedCodec[A]] = c.Expr[D4SDerivedCodec[A]] {
-    q"""new ${weakTypeOf[D4SDerivedCodec[A]]}(_root_.d4s.codecs.D4SEncoder.derived[${weakTypeOf[A]}], _root_.d4s.codecs.D4SDecoder.derived[${weakTypeOf[A]}])"""
+  def materializeCodec[A: c.WeakTypeTag](c: blackbox.Context): c.Expr[D4SDerivedCodec[A]] = c.Expr[D4SDerivedCodec[A]] {
+    import c.universe._
+    q"new ${weakTypeOf[D4SDerivedCodec[A]]}(_root_.d4s.codecs.D4SEncoder.derived[${weakTypeOf[A]}], _root_.d4s.codecs.D4SDecoder.derived[${weakTypeOf[A]}])"
   }
+
+  def materializeAttributeCodec[A: c.WeakTypeTag](c: blackbox.Context): c.Expr[D4SDerivedAttributeCodec[A]] = c.Expr[D4SDerivedAttributeCodec[A]] {
+    import c.universe._
+    q"new ${weakTypeOf[D4SDerivedAttributeCodec[A]]}(_root_.d4s.codecs.D4SAttributeEncoder.derived[${weakTypeOf[A]}], _root_.d4s.codecs.D4SDecoder.derived[${weakTypeOf[A]}])"
+  }
+
 }
 
 final case class D4SDerivedCodec[A](enc: D4SEncoder[A], dec: D4SDecoder[A])
 object D4SDerivedCodec {
   implicit def materialize[A]: D4SDerivedCodec[A] = macro MaterializeDerivationMacros.materializeCodec[A]
+}
+
+final case class D4SDerivedAttributeCodec[A](enc: D4SAttributeEncoder[A], dec: D4SDecoder[A])
+object D4SDerivedAttributeCodec {
+  implicit def materialize[A]: D4SDerivedAttributeCodec[A] = macro MaterializeDerivationMacros.materializeAttributeCodec[A]
 }
