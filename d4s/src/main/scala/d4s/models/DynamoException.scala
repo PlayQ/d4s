a95b90a4b51b814df331a4b5e5ada9edb345b3b0
Alex Liubymov <aliubymov@playq.net>
2020-04-21 11:38:19 +0300

    fix dynamo exception

--- d4s/src/main/scala/d4s/models/DynamoException.scala
+++ d4s/src/main/scala/d4s/models/DynamoException.scala
@@ -11,13 +11,14 @@ object DynamoException {
 
   final case class DynamoQueryException private (override val message: String, override val cause: Throwable) extends DynamoException(message, cause)
   object DynamoQueryException {
-    def apply(cause: Throwable): DynamoQueryException = cause match {
-      case DynamoException(message, cause) => DynamoQueryException(s"Dynamo query failed due to error=$message", cause)
-      case cause                           => DynamoQueryException(s"Dynamo query failed due to error=${cause.getMessage}", cause)
-    }
-    def apply(queryName: String, cause: Throwable): DynamoQueryException = cause match {
-      case DynamoException(message, cause) => new DynamoQueryException(s"Dynamo query `$queryName` failed due to error=$message", cause)
-      case cause                           => new DynamoQueryException(s"Dynamo query `$queryName` failed due to error=${cause.getMessage}", cause)
+    def apply(cause: Throwable): DynamoQueryException                    = from(None, cause)
+    def apply(queryName: String, cause: Throwable): DynamoQueryException = from(Some(queryName), cause)
+    private def from(queryName: Option[String], cause: Throwable) = {
+      val mbName = queryName.fold(" ")(n => s" `$n` ")
+      cause match {
+        case DynamoException(message, cause) => new DynamoQueryException(s"Dynamo query" ++ mbName ++ s"failed due to error: $message", cause)
+        case cause                           => new DynamoQueryException(s"Dynamo query" ++ mbName ++ s"failed due to error: ${cause.getMessage}", cause)
+      }
     }
   }
 }
