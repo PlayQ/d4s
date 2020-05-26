dac357ad1f0dac64e6fb226563ba6561f6087b58
Kai <450507+neko-kai@users.noreply.github.com>
2020-05-26 17:21:23 +0100

    Fix AttributeNames instance for literal tuple, add test, update to izumi 0.10.10, merge envs (#84)
    * Fix AttributeNames instance for literal tuple, add test, update to izumi 0.10.10, merge envs
    
    * Change D4SDockerModule to directly override D4SModule
    
    * warnings

--- d4s-test/src/main/scala/d4s/test/envs/D4SDockerModule.scala
+++ d4s-test/src/main/scala/d4s/test/envs/D4SDockerModule.scala
@@ -1,20 +1,22 @@
 package d4s.test.envs
 
 import d4s.config.DynamoConfig
-import distage.TagKK
+import d4s.modules.D4SModule
+import distage.{ModuleDef, TagKK}
 import izumi.distage.config.ConfigModuleDef
 import izumi.distage.docker.examples.{DynamoDocker, DynamoDockerModule}
 import izumi.distage.model.definition.Id
 
 class D4SDockerModule[F[+_, +_]: TagKK] extends ConfigModuleDef {
-  include(new DynamoDockerModule[F[Throwable, ?]])
   include(D4SDockerModule.config)
-
-  make[DynamoConfig].from {
-    (cfg: DynamoConfig @Id("test-config"), docker: DynamoDocker.Container) =>
-      val knownAddress = docker.availablePorts.availablePorts(DynamoDocker.primaryPort).head
-      cfg.copy(endpointUrl = Some(s"http://${knownAddress.hostV4}:${knownAddress.port}"))
-  }
+  include(DynamoDockerModule[F[Throwable, ?]])
+  include(D4SModule[F] overridenBy new ModuleDef {
+    make[DynamoConfig].from {
+      (cfg: DynamoConfig @Id("test-config"), docker: DynamoDocker.Container) =>
+        val knownAddress = docker.availablePorts.availablePorts(DynamoDocker.primaryPort).head
+        cfg.copy(endpointUrl = Some(s"http://${knownAddress.hostV4}:${knownAddress.port}"))
+    }
+  })
 }
 
 object D4SDockerModule {
