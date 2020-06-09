136b6a04cb8109fc4819baf4fca2d1e03adf68c6
Kai <450507+neko-kai@users.noreply.github.com>
2020-06-09 21:26:37 +0100

    Specify version at which methods were deprecated

--- d4s-circe/src/main/scala/d4s/codecs/circe/package.scala
+++ d4s-circe/src/main/scala/d4s/codecs/circe/package.scala
@@ -3,12 +3,12 @@ package d4s.codecs
 import io.circe.{Decoder, Encoder}
 
 package object circe {
-  @deprecated("use D4SCirceAttributeEncoder.derived[${T}]")
+  @deprecated("use D4SCirceAttributeEncoder.derived[${T}]", "1.0.8")
   def deriveAttribute[T: Encoder]: D4SAttributeEncoder[T] = D4SCirceAttributeEncoder.derived[T]
-  @deprecated("use D4SCirceEncoder.derived[${T}]")
+  @deprecated("use D4SCirceEncoder.derived[${T}]", "1.0.8")
   def deriveEncoder[T: Encoder.AsObject]: D4SEncoder[T] = D4SCirceEncoder.derived[T]
-  @deprecated("use D4SCirceDecoder.derived[${T}]")
+  @deprecated("use D4SCirceDecoder.derived[${T}]", "1.0.8")
   def deriveDecoder[T: Decoder]: D4SDecoder[T] = D4SCirceDecoder.derived[T]
-  @deprecated("use D4SCirceCodec.derived[${T}]")
+  @deprecated("use D4SCirceCodec.derived[${T}]", "1.0.8")
   def deriveCodec[T: Encoder.AsObject: Decoder]: D4SCodec[T] = D4SCirceCodec.derived[T]
 }
