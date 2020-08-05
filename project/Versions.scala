8da8f3a2a3d0106e0e60d67128bfe4156f9b9ad3
Scala Steward <platform-team@playq.net>
2020-08-05 10:09:53 +0000

    Update zio-interop-cats to 2.1.4.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -26,7 +26,7 @@ object V {
   val cats             = "2.1.1"
   val cats_effect      = "2.1.4"
   val zio              = "1.0.0-RC21-2"
-  val zio_interop_cats = "2.1.3.0-RC16"
+  val zio_interop_cats = "2.1.4.0"
 
   val scalatest                = "3.1.2"
   val scalatestplus_scalacheck = "3.1.2.0"
