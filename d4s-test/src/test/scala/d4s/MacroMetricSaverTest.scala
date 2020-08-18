210deeb0d7c7ae8173d40c94a748da8a118253d1
Kai <450507+neko-kai@users.noreply.github.com>
2020-08-18 21:38:00 +0100

    Try to move MacroMetrics macro to a separate object to stop native-image compile errors when trying to init scala-reflect TypeCreator$

--- d4s-test/src/test/scala/d4s/MacroMetricSaverTest.scala
+++ d4s-test/src/test/scala/d4s/MacroMetricSaverTest.scala
@@ -1,26 +1,26 @@
 package d4s
 
 import izumi.fundamentals.platform.language.Quirks._
-import d4s.metrics.MacroMetricDynamoMeter
-import net.playq.metrics.MacroMetricCounter
 import net.playq.metrics.macrodefs.MacroMetricBase
 import org.scalatest.wordspec.AnyWordSpec
 
 final class MacroMetricSaverTest extends AnyWordSpec {
-  private[this] val str = s"${1} x ${2}"
+  private[this] final val str = s"${1} x ${2}"
   str.discard()
 
   "print an error message mentioning discarder when used with a non-constant type" in {
-    shapeless.test.illTyped("implicitly[MacroMetricCounter[str.type]]", ".*discarded.*")
+    shapeless.test.illTyped("implicitly[net.playq.metrics.MacroMetricCounter[str.type]]", ".*discarded.*")
   }
 
   "print an error message if there is no implicit for mentioned metric" in {
     shapeless.test.illTyped("implicitly[MacroMetricSaverTest.TestMetric.MetricBase[str.type, Nothing]]", "import Nothing.discarded._ to disable.*")
-    shapeless.test.illTyped("implicitly[MacroMetricDynamoMeter[str.type]]", ".*import.*MacroMetricsDynamo.discarded._.*")
+    shapeless.test.illTyped("implicitly[d4s.metrics.MacroMetricDynamoMeter[str.type]]", ".*import.*MacroMetricsDynamo.discarded._.*")
   }
 
 }
 
 object MacroMetricSaverTest {
-  object TestMetric extends MacroMetricBase.Counter
+  object TestMetric extends MacroMetricBase.Counter {
+    override object CompileTime extends CompileTime
+  }
 }
--- d4s/src/main/scala/d4s/metrics/MacroMetricsDynamo.scala
+++ d4s/src/main/scala/d4s/metrics/MacroMetricsDynamo.scala
@@ -7,12 +7,14 @@ import scala.language.experimental.macros
 object MacroMetricsDynamo {
   object Meter extends MacroMetricBase.Meter {
     override def createLabel(label: String): String = s"dynamo:request-error:$label"
-    implicit def matMeter[S <: String]: MetricBase[S, MacroMetricsDynamo.discarded.type] = macro materializeImpl[S, MacroMetricsDynamo.discarded.type]
+    implicit def matMeter[S <: String]: MetricBase[S, MacroMetricsDynamo.discarded.type] = macro CompileTime.materializeImpl[S, MacroMetricsDynamo.discarded.type]
+    override object CompileTime extends CompileTime
   }
 
   object Timer extends MacroMetricBase.Timer {
     override def createLabel(label: String): String = s"dynamo:$label:timer"
-    implicit def matTimer[S <: String]: MetricBase[S, MacroMetricsDynamo.discarded.type] = macro materializeImpl[S, MacroMetricsDynamo.discarded.type]
+    implicit def matTimer[S <: String]: MetricBase[S, MacroMetricsDynamo.discarded.type] = macro CompileTime.materializeImpl[S, MacroMetricsDynamo.discarded.type]
+    override object CompileTime extends CompileTime
   }
 
   object discarded {
