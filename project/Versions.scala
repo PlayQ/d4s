9f696c8b41bf72596704d13966ba6a01e00f0000
Scala Steward <platform-team@playq.net>
2020-08-22 10:09:19 +0000

    Update scalatest to 3.2.2

--- project/Versions.scala
+++ project/Versions.scala
@@ -28,6 +28,6 @@ object V {
   val zio              = "1.0.1"
   val zio_interop_cats = "2.1.4.0"
 
-  val scalatest                = "3.2.1"
+  val scalatest                = "3.2.2"
   val scalatestplus_scalacheck = "3.2.1.0"
 }
