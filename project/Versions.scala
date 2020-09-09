e71937fe610b66f68b2c064c4ef5ce4e5f07acef
PlayQPlatformTeam <platform-team@playq.net>
2020-09-09 06:15:35 -0700

    Update cats-effect to 2.2.0 (#155)

--- project/Versions.scala
+++ project/Versions.scala
@@ -24,7 +24,7 @@ object V {
   val magnolia_version = "0.16.0"
 
   val cats             = "2.1.1"
-  val cats_effect      = "2.1.4"
+  val cats_effect      = "2.2.0"
   val zio              = "1.0.1"
   val zio_interop_cats = "2.1.4.0"
 
