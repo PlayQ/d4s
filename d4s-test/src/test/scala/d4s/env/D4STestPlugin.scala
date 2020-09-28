518563a8c01f0a41fb705f3778ab20814f6e4f3f
Kai <450507+neko-kai@users.noreply.github.com>
2020-09-28 13:06:46 +0100

    unused import

--- d4s-test/src/test/scala/d4s/env/D4STestPlugin.scala
+++ d4s-test/src/test/scala/d4s/env/D4STestPlugin.scala
@@ -2,7 +2,6 @@ package d4s.env
 
 import d4s.test.envs.D4SDockerModule
 import izumi.distage.docker.modules.DockerSupportModule
-import izumi.distage.effect.modules.ZIODIEffectModule
 import izumi.distage.plugins.PluginDef
 import logstage.LogBIO
 import net.playq.aws.tagging.modules.AwsTagsModule
