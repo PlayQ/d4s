087bb7dbae4f996475131f1e8ba56b2fc1565d0e
Scala Steward <platform-team@playq.net>
2020-10-09 10:08:53 +0000

    Update zio to 1.0.3

--- project/Versions.scala
+++ project/Versions.scala
@@ -26,7 +26,7 @@ object V {
 
   val cats             = "2.1.1"
   val cats_effect      = "2.2.0"
-  val zio              = "1.0.1"
+  val zio              = "1.0.3"
   val zio_interop_cats = "2.1.4.0"
 
   val scalatest                = "3.2.2"
