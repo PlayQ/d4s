cfb962b7301d883660affbafdd07e72419d6d33e
Alex Liubymov <aliubymov@playq.net>
2020-10-19 17:13:29 +0300

    Set cats=2.2.0 and zio_interop_cats=2.2.0.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -24,10 +24,10 @@ object V {
 
   val magnolia_version = "0.16.0"
 
-  val cats             = "2.1.1"
+  val cats             = "2.2.0"
   val cats_effect      = "2.2.0"
   val zio              = "1.0.3"
-  val zio_interop_cats = "2.1.4.1"
+  val zio_interop_cats = "2.2.0.1"
 
   val scalatest                = "3.2.2"
   val scalatestplus_scalacheck = "3.2.2.0"
