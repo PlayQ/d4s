3f8118a3d403efb67c0420ca0b663d513e19fcd0
Scala Steward <platform-team@playq.net>
2021-02-17 10:11:38 +0000

    Update cats-effect to 2.3.2

--- project/Versions.scala
+++ project/Versions.scala
@@ -25,7 +25,7 @@ object V {
   val magnolia_version = "0.16.0"
 
   val cats             = "2.2.0"
-  val cats_effect      = "2.3.1"
+  val cats_effect      = "2.3.2"
   val zio              = "1.0.4-2"
   val zio_interop_cats = "2.3.1.0"
 
