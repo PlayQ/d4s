5464a70384505ef8082045d5f3c43f636ed52044
PlayQPlatformTeam <platform-team@playq.net>
2021-02-21 04:44:01 -0800

    Update fs2-io to 2.5.3 (#222)

--- project/Versions.scala
+++ project/Versions.scala
@@ -15,7 +15,7 @@ object V {
   val scalacheck           = "1.15.2"
   val scalacheck_shapeless = "1.2.5"
 
-  val fs2 = "2.5.2"
+  val fs2 = "2.5.3"
 
   // Izumi Deps
   val circe                = "0.13.0"
