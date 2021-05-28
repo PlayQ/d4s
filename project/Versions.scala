d19ebfd8e944415afb344468d0b05cf9dc55a91f
Scala Steward <platform-team@playq.net>
2021-05-28 10:08:48 +0000

    Update scala-library, scala-reflect to 2.12.14

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,5 +1,5 @@
 object V {
-  val scala_212               = "2.12.13"
+  val scala_212               = "2.12.14"
   val scala_213               = "2.13.5"
   val scala_collection_compat = "2.4.4"
   val izumi_version           = "1.0.6"
