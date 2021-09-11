282d58b9e3ba48388fbdf6d907e181eccbcc0183
Scala Steward <platform-team@playq.net>
2021-09-11 10:06:51 +0000

    Update fs2-io to 3.1.2

--- project/Versions.scala
+++ project/Versions.scala
@@ -14,7 +14,7 @@ object V {
   val scalacheck           = "1.15.2"
   val scalacheck_shapeless = "1.2.5"
 
-  val fs2 = "2.5.9"
+  val fs2 = "3.1.2"
 
   // Izumi Deps
   val circe                = "0.14.1"
